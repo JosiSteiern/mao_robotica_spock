7151577a378b16d43a9fc5210681afbc4aa7be21
Nome Exemplo <josianesteiernagel@gmail.com>
2024-11-04 11:27:10 -0300

    novas alterações

--- portifolio S.P.O.C.K/spock-v1/spock-v1.js
+++ portifolio S.P.O.C.K/spock-v1/spock-v1.js
@@ -1,45 +1,45 @@
 // Evento de hover para mostrar o texto nas imagens
 // imagem 1
-document.getElementById("cont-2-img-1").addEventListener("mouseover", ()=>{
+document.getElementById("cont-2-img-1").addEventListener("mouseenter", ()=>{
     document.getElementById("descricao-imagem1").style.display = "flex";
 })
 
-document.getElementById("cont-2-img-1").addEventListener("mouseout", ()=>{
+document.getElementById("cont-2-img-1").addEventListener("mouseleave", ()=>{
     document.getElementById("descricao-imagem1").style.display = "none";
 })
 
 // imagem 2
-document.getElementById("cont-2-img-2").addEventListener("mouseover", ()=>{
+document.getElementById("cont-2-img-2").addEventListener("mouseenter", ()=>{
     document.getElementById("descricao-imagem2").style.display = "flex";
 })
 
-document.getElementById("cont-2-img-2").addEventListener("mouseout", ()=>{
+document.getElementById("cont-2-img-2").addEventListener("mouseleave", ()=>{
     document.getElementById("descricao-imagem2").style.display = "none";
 })
 
 // imagem 3
-document.getElementById("cont-2-img-3").addEventListener("mouseover", ()=>{
+document.getElementById("cont-2-img-3").addEventListener("mouseenter", ()=>{
     document.getElementById("descricao-imagem3").style.display = "flex";
 })
 
-document.getElementById("cont-2-img-3").addEventListener("mouseout", ()=>{
+document.getElementById("cont-2-img-3").addEventListener("mouseleave", ()=>{
     document.getElementById("descricao-imagem3").style.display = "none";
 })
 
 // imagem 4
-document.getElementById("cont-2-img-4").addEventListener("mouseover", ()=>{
+document.getElementById("cont-2-img-4").addEventListener("mouseenter", ()=>{
     document.getElementById("descricao-imagem4").style.display = "flex";
 })
 
-document.getElementById("cont-2-img-4").addEventListener("mouseout", ()=>{
+document.getElementById("cont-2-img-4").addEventListener("mouseleave", ()=>{
     document.getElementById("descricao-imagem4").style.display = "none";
 })
 
 // imagem 5
-document.getElementById("cont-2-img-5").addEventListener("mouseover", ()=>{
+document.getElementById("cont-2-img-5").addEventListener("mouseenter", ()=>{
     document.getElementById("descricao-imagem5").style.display = "flex";
 })
 
-document.getElementById("cont-2-img-5").addEventListener("mouseout", ()=>{
+document.getElementById("cont-2-img-5").addEventListener("mouseleave", ()=>{
     document.getElementById("descricao-imagem5").style.display = "none";
 })
---
+++ portifolio S.P.O.C.K/spock-v2/spock-v2.js
@@ -0,0 +1,27 @@
+// Evento de hover para mostrar o texto nas imagens
+// imagem 1
+document.getElementById("imagem-linha2-1").addEventListener("mouseenter", ()=>{
+    document.getElementById("descricao-img-1").style.display = "block";
+})
+
+document.getElementById("imagem-linha2-1").addEventListener("mouseleave", ()=>{
+    document.getElementById("descricao-img-1").style.display = "none";
+})
+
+// imagem 2
+document.getElementById("imagem-linha2-2").addEventListener("mouseenter", ()=>{
+    document.getElementById("descricao-img-2").style.display = "block";
+})
+
+document.getElementById("imagem-linha2-2").addEventListener("mouseleave", ()=>{
+    document.getElementById("descricao-img-2").style.display = "none";
+})
+
+// imagem 3
+document.getElementById("imagem-linha2-3").addEventListener("mouseenter", ()=>{
+    document.getElementById("descricao-img-3").style.display = "block";
+})
+
+document.getElementById("imagem-linha2-3").addEventListener("mouseleave", ()=>{
+    document.getElementById("descricao-img-3").style.display = "none";
+})
